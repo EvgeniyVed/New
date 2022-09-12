58409d750f7e4316f3ccb86cdb939d6d3de96f9e
Vedernikov <vedernikov@gmail.com>
2022-09-12 11:52:45 +0300

    добавленна 3 задача

---
+++ Zadacha3_23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>0)
+{
+    for (int i = 1;i<=n;i++)
+    Console.Write("{0}, ",i*i*i);
+} else
+{
+    Console.WriteLine("Введено не корректное число");
+}

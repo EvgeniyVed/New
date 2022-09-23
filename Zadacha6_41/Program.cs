233b4cf877f1d8d442749231b31beea30009996a
Vedernikov <vedernikov@gmail.com>
2022-09-23 15:12:19 +0300

    ДЗ №6

---
+++ Zadacha6_41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите количество чисел");
+int M = Convert.ToInt32(Console.ReadLine());
+if (M>0)
+{
+    int pozitiv = 0;
+    int[] numbers = new int[M];
+    Console.WriteLine("Вводите числа");
+    for (int i=0;i<M;i++)
+    {
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+        if (numbers[i]>0)
+        pozitiv++; 
+    } 
+    Console.WriteLine("Количество положительных чисел:");
+    Console.WriteLine(pozitiv);    
+} else
+{
+    Console.WriteLine("Введено не корректное количество");
+}
---
+++ Zadacha6_43/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("Введите значения b1, k1, b2 и k2");
+int b1 = Convert.ToInt32(Console.ReadLine());
+int k1 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+int k2 = Convert.ToInt32(Console.ReadLine());
+double xChisl = (b1-b2);
+double xZnamen = (k2-k1);
+double x = xChisl/xZnamen;
+double y = x*k1+b1;
+Console.WriteLine("Точка пересечения двух прямых ({0};{1})",x,y);
